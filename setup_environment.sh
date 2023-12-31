12115532342d9eba9b64288a169dad8f9bb54165
Suraj Joshi <47292337+surajpjoshi@users.noreply.github.com>
2023-12-31 16:50:42 +0530

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo su
 sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
